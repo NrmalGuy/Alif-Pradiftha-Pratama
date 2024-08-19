b65b13b9302df6d0341bfdf29a8d749b3f4d8b4d
FTURA <abiauliaibrahim223@Gmail.com>
2024-08-19 23:33:13 +0700

    terbaru

--- aos.js
+++ aos.js
@@ -1,1 +1,17 @@
 AOS.init();
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM loaded');
+    AOS.init();
+    console.log('AOS initialized');
+    window.addEventListener('scroll', function() {
+      console.log('Scroll event triggered');
+      var scrollPosition = window.scrollY;
+      var disappearText = document.getElementById('disappear-text');
+      if (scrollPosition > 200) {
+        disappearText.style.opacity = 0;
+      } else {
+        disappearText.style.opacity = 1;
+      }
+    });
+  });
